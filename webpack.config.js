a8444b683adf2bb3778bd1fa5ee3a3aee056dc46
Daiki Saito <Daiki.Saito@isobar.com>
2018-08-21 14:52:13 +0900

    webpack 設定修正

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['env', 'react']
+                            presets: ['env']
                         }
                     }
                 ],
